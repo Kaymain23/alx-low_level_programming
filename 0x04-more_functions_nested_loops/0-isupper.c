55ba48faf6bec47167c1dd4887ccc9be85b39126
Kaymain23 <kaymain23@gmail.com>
2022-09-15 10:39:47 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,13 @@
 /**
 *_isupper - function that verifies if a  character is uppercase or not
 *@c: tested character
-*Return: returns 1 if cis uppercase , 0 if not otherwise
+*Return: returns 1 if c is uppercase , 0 if not otherwise
 */
 
 int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
 return (0);
 }
