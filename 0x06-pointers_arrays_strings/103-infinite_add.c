8806220d6ee2a9ad1f447ed092b9d8f075593dba
Kaymain23 <kaymain23@gmail.com>
2022-09-23 15:47:04 -0700

    Advanced task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,60 +1,50 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-* infinite_add - C function that adds two numbers stored
-*in strings to a buffer.
-*Assumes that strings are never empty and
-*that numbers will always be positive, or 0.
-*Assumes there are only digits stored in the number strings.
-*If result can be stored in the buffer,
-*returns a pointer to the result.
-*If result cannot be stored in the buffer, returns `0`.
-*@n1:first number to be added
-*@n2:second number to be added
-*@r: store result
-*@size_r: size of buffer
-*Return:returns pointer to result
-*/
-
+ * infinite_add - adds two numbers in strings
+ * @n1: type string 1
+ * @n2: type string 2
+ * @r: type buffer string.
+ * @size_r: buffer size:
+ * Return: pointer dest.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-}
+	int len1, len2, strg, d2, d3, s3;
 
-/**
-* add_strings - Adds the numbers stored in two strings.
-* @n1: The string containing the first number to be added.
-* @n2: The string containing the second number to be added.
-* @r: The buffer to store the result.
-* @r_index: The current index of the buffer.
-*
-* Return: If r can store the sum - a pointer to the result.
-*         If r cannot store the sum - 0.
-*/
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-	int num, tens = 0;
-
-	for (; *n1 && *n2; n1--, n2--, r_index--)
+	d3 = s3 = 0;
+/* defining length of n1 n2 with bigger */
+	for (len1 = 0; n1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; n2[len2] != '\0'; len2++)
+		;
+/* equal the longitude of len to buffer bigger */
+	if (len1 >= len2)
+		strg = c1;
+	if (size_r <= strg)
+		return (0);
+	r [bg + 1]
+	len1 = len1 - 1;
+	len2 = len2 - 1;
+/* defining the longitude in 2 digits to store it in buffer */
+	while (len1 >= 0 || len2 >= 0)
 	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		d1 = d2 = 0;
+		if (len1 >= 0)
+			d1 = n1[len1--] - '0';
+		if (len2 >= 0)
+		d2 = n2[len2--] - '0';
+/* sum of d1 and d2 and the carry s3 */
+		d3 = d1 + d2 + s3;
+/* new condition */
+		if (d3 > 9)
+		{
+			s3 = 1;
+			s3 = s3 - 10;
+		}
+		else
+			s3 = 0;
+r[]
 	}
-
-	for (; *n1; n1--; r_index++)
-	{
-		num = *(n1 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	for (; *n2; n2--;  r_index--)
-	{
-		num = (*n2 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10; 
-	}
-	
+		return (0);
 }
